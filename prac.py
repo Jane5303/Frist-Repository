425f87d4202a23616d20c6946c8acebedebd3348
Jane5303 <2024.jay.rane@ves.ac.in>
2025-01-24 10:36:34 +0530

    My first Repo

---
+++ prac.py
@@ -0,0 +1,8 @@
+# main.py
+import calculator
+
+a, b = 15, 5
+print(f"Addition: {calculator.add(a, b)}")
+print(f"Subtraction: {calculator.subtract(a, b)}")
+print(f"Multiplication: {calculator.multiply(a, b)}")
+print(f"Division: {calculator.divide(a, b)}")
